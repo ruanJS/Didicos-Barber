b676f6f85c6faea046201ca317b860abf45176d8
devguedess <ruan.guedes@vitalisinvest.com.br>
2025-06-19 13:38:52 -0300

    feat: refatorar index.vue para usar script setup e adicionar metadados Open Graph; criar arquivo robots.txt

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,41 @@
 <template>
-    <HeroSection />
-    <AboutSection />
-    <ServicesSection />
-    <TeamSection />
-    <UnitsSection />
-    <MapsSection />
-    <SocialSection />
-    <FloatingButtons />
-    <Footer />
-
+  <HeroSection />
+  <AboutSection />
+  <ServicesSection />
+  <TeamSection />
+  <UnitsSection />
+  <MapsSection />
+  <SocialSection />
+  <FloatingButtons />
+  <Footer />
 </template>
-<script lang="ts">
-import HeroSection from '~/components/HeroSection.vue';
-import AboutSection from '~/components/AboutSection.vue';
-import ServicesSection from '~/components/ServicesSection.vue';
-import FloatingButtons from '~/components/FloatingButtons.vue';
-import MapsSection from '~/components/MapsSection.vue';
-import Footer from '~/components/Footer.vue';
-import TeamSection from '~/components/TeamSection.vue';
-import UnitsSection from '~/components/UnitsSection.vue';
-import SocialSection from '~/components/SocialSection.vue';
+
+<script lang="ts" setup>
+import HeroSection from '~/components/HeroSection.vue'
+import AboutSection from '~/components/AboutSection.vue'
+import ServicesSection from '~/components/ServicesSection.vue'
+import FloatingButtons from '~/components/FloatingButtons.vue'
+import MapsSection from '~/components/MapsSection.vue'
+import Footer from '~/components/Footer.vue'
+import TeamSection from '~/components/TeamSection.vue'
+import UnitsSection from '~/components/UnitsSection.vue'
+import SocialSection from '~/components/SocialSection.vue'
 
 useSeoMeta({
   title: "Didico's Barber - Barbearia Premium",
-  ogTitle: "Didico's Barber - Barbearia Premium",
   description: "Agende seu horário e venha conhecer o melhor corte de cabelo da região do Morumbi.",
-  ogDescription: "Agende seu horário e venha conhecer o melhor corte de cabelo da região do Morumbi.",
-  ogImage: '/img/logo.png',
-  twitterCard: 'summary_large_image',
-})
+  keywords: 'barbearia, corte de cabelo, fade, barba, cabelo masculino, barbearia no Morumbi',
+  author: "Didico's Barber",
+  viewport: 'width=device-width, initial-scale=1',
 
+  ogTitle: "Didico's Barber - Barbearia Premium",
+  ogDescription: "O melhor corte da região do Morumbi. Agende seu horário.",
+  ogImage: '/public/img/logo.png',
+  ogUrl: 'https://didicosbarber.vercel.app',
+  ogType: 'website',
+
+  twitterCard: 'summary_large_image',
+  twitterSite: '@didicosbarber',
+  twitterCreator: '@devguedess_'
+})
 </script>
